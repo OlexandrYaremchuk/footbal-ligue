6ecd907d78669656d90018720bd81608144277f7
Egnarok-dev <124812439+Egnarok-dev@users.noreply.github.com>
2023-10-04 20:26:49 +0300

    add new loader component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from '../style/loading.module.css'
 const Loading = () => {
     return (
-        <div style={{width:"180px", margin:"25px auto 100px", borderBottom:"10px solid green"}}>
+        <div style={{width:"180px", margin:"25px auto 100px", borderBottom:"8px solid green"}}>
             <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className={style.wheel_and_hamster}>
                 <div className={style.wheel}></div>
                 <div className={style.hamster}>
