931cfc7c0ce66c05e2164c5f669a22b77f57194b
Grabenko <Palya2293@gmail.com>
2023-10-04 20:19:36 +0300

    add Title/img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import League from "./League";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
 <League/>
+<Home/>
     </div>
   );
 }
